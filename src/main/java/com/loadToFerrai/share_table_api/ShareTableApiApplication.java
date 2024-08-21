761a6aca6f587fee7309277335cbdd214761ac02
이희준 <syber911911@naver.com>
2024-08-21 16:31:02 +0900

    set spring boot project

---
+++ src/main/java/com/loadToFerrai/share_table_api/ShareTableApiApplication.java
@@ -0,0 +1,13 @@
+package com.loadToFerrai.share_table_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareTableApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareTableApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/loadToFerrai/share_table_api/ShareTableApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.loadToFerrai.share_table_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareTableApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
