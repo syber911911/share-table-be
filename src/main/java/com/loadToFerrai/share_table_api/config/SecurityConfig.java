2888b1407195103e9b0361670b53a2ad4aee9310
syber911911 <syber911911@gmail.com>
2024-10-16 22:06:45 +0900

    chore: deplpy script 작성중

--- src/main/java/com/loadToFerrai/share_table_api/config/SecurityConfig.java
+++ src/main/java/com/loadToFerrai/share_table_api/config/SecurityConfig.java
@@ -16,7 +16,7 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(
                 request -> request.requestMatchers(PathRequest.toH2Console()).permitAll()
-                        .anyRequest().authenticated()
+                        .anyRequest().permitAll()
                 )
                 .csrf(
                         csrf -> csrf.ignoringRequestMatchers(PathRequest.toH2Console())
