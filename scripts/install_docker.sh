021f3a91ffa93034b1a186f57330615df0c14512
syber911911 <syber911911@gmail.com>
2024-10-22 17:35:33 +0900

    chore: CodeDeploy Script test

--- scripts/install_docker.sh
+++ scripts/install_docker.sh
@@ -9,11 +9,11 @@ else
     sudo apt-get install -y docker.io
 fi
 
-## Docker Compose 설치 및 업데이트
-#echo "Docker Compose 설치 중입니다..."
-#sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-#sudo chmod +x /usr/local/bin/docker-compose
-#
-#echo "설치가 완료되었습니다. 현재 버전:"
-#docker --version
-#docker-compose --version
+# Docker Compose 설치 및 업데이트
+echo "Docker Compose 설치 중입니다..."
+sudo curl -L "https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+echo "설치가 완료되었습니다. 현재 버전:"
+docker --version
+docker-compose --version
